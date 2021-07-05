95bf07077f61b9b5b1be1224777c2d48feda3492
Benjamin Maudslay <benjaminmaudslay@Benjamins-MacBook-Pro-144.local>
2021-07-05 11:46:04 +0100

    error handling added

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,24 @@ import "./App.css";
 
 const App = () => {
   const [data, setData] = useState("");
+  const [error, setError] = useState({ error: false, message: "" });
 
   const handleFetch = async () => {
-    // fetch("https://tronalddump.io/random/quote")
-    //   .then((res) => res.json())
-    //   .then((data) => console.log(data));
-    const response = await fetch("https://tronalddump.io/random/quote");
-    const data = await response.json();
-    setData(data);
+    try {
+      const response = await fetch("https://tronalddump.io/random/quote");
+      if (response.status !== 200) {
+        throw new Error("Oops!");
+      }
+      const data = await response.json();
+      setData(data);
+    } catch (error) {
+      setError({ error: true, message: error.message });
+    }
   };
 
+  if (error.error) {
+    return <h1>An error has occured: {error.message}</h1>;
+  }
   return (
     <div>
       <h1>Tronald Dump Quote Machine</h1>
