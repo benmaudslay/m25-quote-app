ce5bfd5a4cb8e1bccaf2d275c79940e8400db4ae
Benjamin Maudslay <benjaminmaudslay@Benjamins-MacBook-Pro-144.local>
2021-07-05 11:23:12 +0100

    added fetch request

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [quote, setQuote] = useState("");
+
+  const handleFetch = async () => {
+    // fetch("https://tronalddump.io/random/quote")
+    //   .then((res) => res.json())
+    //   .then((data) => console.log(data));
+    const response = await fetch("https://tronalddump.io/random/quote");
+    const data = await response.json();
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <h1>Tronald Dump Quote Machine</h1>
+      <p>{quote}</p>
+      <button onClick={handleFetch}>Click</button>
+    </div>
+  );
+};
+
+export default App;
