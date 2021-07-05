526fbc4f3d6e2a5a14006e6862494d624389c20c
Benjamin Maudslay <benjaminmaudslay@Benjamins-MacBook-Pro-144.local>
2021-07-05 11:26:26 +0100

    display fetched data

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [quote, setQuote] = useState("");
+  const [data, setData] = useState("");
 
   const handleFetch = async () => {
     // fetch("https://tronalddump.io/random/quote")
@@ -10,13 +10,13 @@ const App = () => {
     //   .then((data) => console.log(data));
     const response = await fetch("https://tronalddump.io/random/quote");
     const data = await response.json();
-    console.log(data);
+    setData(data);
   };
 
   return (
     <div>
       <h1>Tronald Dump Quote Machine</h1>
-      <p>{quote}</p>
+      <p>{data.value}</p>
       <button onClick={handleFetch}>Click</button>
     </div>
   );
